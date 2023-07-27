ba079a7b9455ce9df7ef4ddf37b237d7c1ae07e0
zhanglun <zhanglun@yiwise.com>
2023-07-27 18:53:30 +0800

    test upload file

--- apps/server/src/books/books.controller.ts
+++ apps/server/src/books/books.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller('books')
 export class BooksController {
   @Get()
   findAll(): [] {
     return [];
+  }
+
+  @Post()
+  addOne(): any {
+    return [];
+  }
+
+  @Post('upload')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadFile(
+    @Body() body: any,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    return {
+      body,
+      file: file.buffer.toString(),
+    };
   }
 }
