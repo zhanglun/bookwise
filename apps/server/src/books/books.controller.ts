836a44d7fd0c4df2bac4f1308251d8e50197b8a2
zhanglun <zhanglun@yiwise.com>
2023-07-28 19:06:16 +0800

    improvement: parse content.opf

--- apps/server/src/books/books.controller.ts
+++ apps/server/src/books/books.controller.ts
@@ -7,12 +7,13 @@ import {
   UploadedFiles,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
-import { unzip } from 'node:zlib';
-import * as AdmZip from 'adm-zip';
-console.log('%c Line:5 🥤 AdmZip', 'color:#f5ce50', AdmZip);
+import { Express } from 'express';
+import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
+  constructor(private booksService: BooksService) {}
+
   @Get()
   findAll(): [] {
     return [];
@@ -29,15 +30,10 @@ export class BooksController {
     @Body() body: any,
     @UploadedFiles() files: Array<Express.Multer.File>,
   ) {
-    console.log(files);
     const { originalname, mimetype, buffer, size } = files[0];
-    const zip = new AdmZip(buffer);
-    const zipEntries = zip.getEntries();
-    console.log(zipEntries.length);
 
-    zipEntries.forEach((z) => {
-      console.log(z.toString());
-    });
+    this.booksService.parseBook(buffer);
+
     return {
       body,
       file: {
--- apps/server/src/books/books.service.ts
+++ apps/server/src/books/books.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import * as AdmZip from "adm-zip";
+import { XMLParser } from "fast-xml-parser";
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  public parseBook(content: Buffer) {
+    const zip = new AdmZip(content);
+    const zipEntries = zip.getEntries();
+
+    zipEntries.forEach((z) => {
+      // console.log(z);
+      const entryData = z.getData();
+      switch (z.entryName) {
+        case 'mimetype':
+          this.parseMimetype(entryData);
+          break;
+        case 'content.opf':
+          this.parseOPF(entryData);
+          break;
+        case 'cover.jpg':
+          break;
+        default:
+          break;
+      }
+    });
+  }
+
+  public parseMimetype(bf: Buffer) {
+    return bf.toString();
+  }
+
+  public parseOPF(bf: Buffer) {
+    const data = bf.toString();
+    const parser = new XMLParser({
+      ignoreAttributes: false,
+      transformTagName: (tagName) => tagName.replace(/dc:/gi, ''),
+    });
+    const info = parser.parse(data);
+
+    return { ...info.package };
+  }
+
+  // public getContentOPF(content: string) {
+  //
+  // }
+}
