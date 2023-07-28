c1d2593039d203fb2b79cf1c83674fe96a303be8
zhanglun <zhanglun@yiwise.com>
2023-07-28 16:47:33 +0800

    chore: test parser

--- apps/server/src/books/books.controller.ts
+++ apps/server/src/books/books.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Post, Body, UseInterceptors, UploadedFiles } from '@nestjs/common';
-import { FilesInterceptor } from "@nestjs/platform-express";
-import { unzip } from "node:zlib";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseInterceptors,
+  UploadedFiles,
+} from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { unzip } from 'node:zlib';
 import * as AdmZip from 'adm-zip';
-console.log("%c Line:5 🥤 AdmZip", "color:#f5ce50", AdmZip);
+console.log('%c Line:5 🥤 AdmZip', 'color:#f5ce50', AdmZip);
 
 @Controller('books')
 export class BooksController {
@@ -18,22 +25,26 @@ export class BooksController {
 
   @Post('upload')
   @UseInterceptors(FilesInterceptor('files'))
-  uploadFile(@Body() body: any, @UploadedFiles() files: Array<Express.Multer.File>) {
+  uploadFile(
+    @Body() body: any,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+  ) {
     console.log(files);
     const { originalname, mimetype, buffer, size } = files[0];
     const zip = new AdmZip(buffer);
     const zipEntries = zip.getEntries();
-    console.log(zipEntries.length)
+    console.log(zipEntries.length);
 
     zipEntries.forEach((z) => {
-      console.log(z.toString())
-    })
+      console.log(z.toString());
+    });
     return {
       body,
       file: {
-        originalname, mimetype, size,
-      }
-    }
+        originalname,
+        mimetype,
+        size,
+      },
+    };
   }
 }
-
