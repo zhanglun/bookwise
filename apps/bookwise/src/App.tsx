8b6b36a8eb8d9a4b7e75ac067acff8edb7cc429f
zhanglun <zhanglun1410@gmail.com>
2023-08-14 18:28:41 +0800

    chore: add icons

--- apps/bookwise/src/App.tsx
+++ apps/bookwise/src/App.tsx
@@ -4,10 +4,18 @@ import { TabBar } from "./components/TabBar";
 import { Sidebar } from "./components/SideBar";
 import { Outlet, useMatch } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
+import {
+  ChevronLeft,
+  ChevronRight,
+  PanelLeftClose,
+  PanelLeftOpen,
+} from "lucide-react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [server, setServer] = useState<any>({});
   const [isReading, setIsReading] = useState(false);
+  const [expanded, setExpanded] = useState(false);
   const match = useMatch("/reader");
 
   console.log("%c Line:10 🥤 match", "color:#4fff4B", match);
@@ -19,18 +27,30 @@ function App() {
   }, []);
 
   useEffect(() => {
+    console.log("=====>");
+
     setIsReading(!!match);
 
-    if(match) {
-      const $sidebar = document.querySelector('#sidebar');
-      const $catalog = document.querySelector('#catalog');
+    if (match) {
+      const $sidebar = document.querySelector("#sidebar");
+      const $catalog = document.querySelector("#catalog");
 
-      const animateSidebar = () => {
-        console.log("%c Line:28 🥝 animateSidebar", "color:#33a5ff");
-        $catalog && animate($catalog, { x: [0, 100] }, { type: "spring" })
-      }
+      const animateSidebarIn = () => {
+        if ($catalog && !expanded) {
+          animate($catalog, { x: [0, 100] }, { type: "spring" });
+          setExpanded(true);
+        }
+      };
 
-      $sidebar?.addEventListener('mouseover', animateSidebar);
+      const animateSidebarOut = () => {
+        if ($catalog) {
+          animate($catalog, { x: [100, 0] }, { type: "spring" });
+          setExpanded(false);
+        }
+      };
+
+      // $sidebar?.addEventListener("click", animateSidebarIn, true);
+      // $sidebar?.addEventListener("mouseout", animateSidebarOut, true);
     }
   }, [match]);
 
@@ -41,8 +61,23 @@ function App() {
         id="app"
         className="w-full h-full grid grid-rows-[30px_minmax(0,1fr)] gap-2 backdrop-blur-[40px]"
       >
-        {/* <div></div> */}
-        <TabBar />
+        <div className="grid grid-flow-col grid-cols-[auto_1fr_auto] items-center">
+          <div className="grid grid-flow-col gap-[2px]">
+            <Button size="icon" variant="ghost" className="w-8 h-8">
+              <PanelLeftClose size={18} />
+            </Button>
+            <Button size="icon" variant="ghost" className="w-8 h-8">
+              <PanelLeftOpen size={18} />
+            </Button>
+            <Button size="icon" variant="ghost" className="w-8 h-8">
+              <ChevronLeft size={18} />
+            </Button>
+            <Button size="icon" variant="ghost" className="w-8 h-8">
+              <ChevronRight size={18} />
+            </Button>
+          </div>
+        </div>
+        {/* <TabBar /> */}
         <div className="grid gap-4 grid-cols-[260px_1fr]">
           {isReading}
           <Sidebar />
--- apps/bookwise/src/pages/Reader/Catalog.tsx
+++ apps/bookwise/src/pages/Reader/Catalog.tsx
@@ -1,56 +1,77 @@
-import clsx from "clsx";
-import {BookCatalog} from "@/helpers/parseEpub";
+import { useState } from "react";
 import { ChevronsLeft } from "lucide-react";
+import clsx from "clsx";
+import { animate } from "framer-motion";
+import { BookCatalog } from "@/helpers/parseEpub";
 
 export interface CatalogProps {
   data: BookCatalog[];
-    packaging: any;
+  packaging: any;
   onGoToPage: (href: string, id: string) => void;
 }
 
 export const Catalog = (props: CatalogProps) => {
   const { data, packaging, onGoToPage } = props;
   const { metadata } = packaging;
+  const [expanded, setExpanded] = useState(false);
 
   const goToPage = (href: string, id: string) => {
     onGoToPage(href, id);
   };
 
+  const showSidebar = () => {
+    const $catalog = document.querySelector("#catalog");
+
+    if ($catalog && !expanded) {
+      animate($catalog, { x: [0, 240] });
+      setExpanded(true);
+    }
+  };
+
   const renderItems = (list: BookCatalog[], idx = 0) => {
-    const styles = ["font-semibold mx-2 my-2 py-1", "font-normal ml-6 mr-3 py-1"];
+    const styles = [
+      "font-semibold mx-2 my-2 py-1",
+      "font-normal ml-6 mr-3 py-1",
+    ];
 
     return list.map((item) => {
       const { id, label, href, subitems } = item;
       return (
+        <div className={clsx("text-sm cursor-default")} key={id}>
           <div
-              className={clsx("text-sm cursor-default")}
-              key={id}
-          >
-            <div
-                data-idx={idx}
-                data-href={href}
-                className={clsx("hover:underline overflow-hidden text-ellipsis whitespace-nowrap", styles[idx])}
-                onClick={() => goToPage(href, id)}
-            >
-              {label}
-            </div>
-            {subitems.length > 0 && (
-                <div className="">{renderItems(subitems, idx + 1)}</div>
+            data-idx={idx}
+            data-href={href}
+            className={clsx(
+              "hover:underline overflow-hidden text-ellipsis whitespace-nowrap",
+              styles[idx]
             )}
+            onClick={() => goToPage(href, id)}
+          >
+            {label}
           </div>
+          {subitems.length > 0 && (
+            <div className="">{renderItems(subitems, idx + 1)}</div>
+          )}
+        </div>
       );
     });
   };
 
   return (
-    <div id="catalog" className="grid grid-flow-row w-[250px] bg-white shadow-sm rounded-md border border-[#efefef] border-opacity-60 absolute top-[46px] left-[20px] bottom-2">
+    <div
+      id="catalog"
+      className="grid grid-flow-row w-[250px] bg-white shadow-sm rounded-md border border-[#efefef] border-opacity-60 absolute top-[46px] left-[20px] bottom-2"
+    >
       <div className="grid grid-flow-col grid-cols-[24px_1fr] gap-1 items-center px-4 py-2 mt-3">
-          <span className="w-6 h-6 rounded-lg hover:bg-accent flex items-center justify-center">
-            <ChevronsLeft size={18} />
-          </span>
-          <span className="text-sm font-bold overflow-hidden whitespace-nowrap text-ellipsis">
+        <span
+          className="w-6 h-6 rounded-lg hover:bg-accent flex items-center justify-center"
+          onClick={showSidebar}
+        >
+          <ChevronsLeft size={18} />
+        </span>
+        <span className="text-sm font-bold overflow-hidden whitespace-nowrap text-ellipsis">
           {metadata.title}
-          </span>
+        </span>
       </div>
       <div className="px-2 py-2 h-full overflow-y-scroll">
         {renderItems(data)}
