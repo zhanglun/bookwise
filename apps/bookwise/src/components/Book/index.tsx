b58aa6116666ac18511307c1185792e28d36ecf4
zhanglun <zhanglun1410@gmail.com>
2023-08-16 18:27:45 +0800

    improvement: update flex layout

--- apps/bookwise/src/components/Book/index.tsx
+++ apps/bookwise/src/components/Book/index.tsx
@@ -14,17 +14,15 @@ export const Book = (props: BookProps) => {
     <div
       className="border border-border rounded-lg isolation bg-white w-[208px] transition-transform hover:scale-[1.02] hover:shadow-hover-book"
     >
-      <div className="h-[308px]">
+      <div className="h-[308px] flex flex-col">
         <div className="w-full h-[202px] flex items-center justify-center relative opacity-90">
           <Cover onClick={ () => onCoverClick(data) } book={ data }/>
         </div>
-        <div className="flex-1 p-4 max-w-p[">
-          <div
-            className="text-sm font-bold text-ellipsis overflow-hidden whitespace-nowrap"
-          >
-            { data.title }
-          </div>
-          <div className="flex flex-row items-center text-xs leading text-muted-foreground">
+        <div className="flex-1 p-3 flex flex-col border-t border-border">
+          <h1
+            className="text-sm font-bold line-clamp-2 leading-5 mb-1 flex-1 balance"
+          >{ data.title }</h1>
+          <div className="leading-5 flex flex-row items-center text-xs leading text-muted-foreground">
            <span className="flex-1 text-ellipsis overflow-hidden whitespace-nowrap">
              { data.author.name }
            </span>
