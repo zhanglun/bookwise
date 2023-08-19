0500a9341cc3a48fbe12f933b8f585191a326dc4
zhanglun <zhanglun1410@gmail.com>
2023-08-19 22:52:46 +0800

    feat: redesign layout

--- apps/bookwise/src/App.tsx
+++ apps/bookwise/src/App.tsx
@@ -2,15 +2,8 @@ import { useCallback, useEffect, useState } from "react";
 import { useAnimate, motion } from "framer-motion";
 import { TabBar } from "./components/TabBar";
 import { Sidebar } from "./components/SideBar";
-import { Outlet, useMatch, useNavigate } from "react-router-dom";
+import { Outlet, useMatch } from "react-router-dom";
 import { Toaster } from "./components/ui/toaster";
-import {
-  ChevronLeft,
-  ChevronRight,
-  PanelLeftClose,
-  PanelLeftOpen, PanelRight,
-} from "lucide-react";
-import { Button } from "./components/ui/button";
 import { useBearStore } from "./store";
 import clsx from "clsx";
 
@@ -20,14 +13,9 @@ function App() {
     updateSidebarCollapse: state.updateSidebarCollapse,
     bookStack: state.bookStack,
   }));
-  const navigate = useNavigate();
-  const [scope, animate] = useAnimate();
   const [server, setServer] = useState<any>({});
   const [isReading, setIsReading] = useState(false);
   const match = useMatch("/reader");
-  const toggleSidebar = () => {
-    store.updateSidebarCollapse();
-  };
 
   useEffect(() => {
     window.electronAPI?.onUpdateServerStatus((_event: any, value: any) => {
@@ -40,81 +28,27 @@ function App() {
   }, [store.sidebarCollapse, match]);
 
   useEffect(() => {
-    console.log('====>');
+    console.log("====>");
     console.log(store.bookStack);
-  }, [store.bookStack])
+  }, [store.bookStack]);
 
   return (
     <>
       <Toaster />
-      <div
+      <motion.div
+        layout
         id="app"
-        className="w-full h-full grid grid-rows-[30px_minmax(0,1fr)] gap-2 backdrop-blur-[40px]"
+        className="w-full h-full grid grid-cols-[minmax(0,max-content)_1fr] backdrop-blur-[40px]"
       >
-        <div className="grid grid-flow-col grid-cols-[auto_1fr_auto] items-center">
-          <div className="grid grid-flow-col gap-[2px]">
-            {!store.sidebarCollapse && (
-              <Button
-                size="icon"
-                variant="ghost"
-                className="w-8 h-8"
-                onClick={toggleSidebar}
-              >
-                <PanelLeftClose size={18} />
-              </Button>
-            )}
-            {store.sidebarCollapse && (
-              <Button
-                size="icon"
-                variant="ghost"
-                className="w-8 h-8"
-                onClick={toggleSidebar}
-              >
-                <PanelLeftOpen size={18} />
-              </Button>
-            )}
-            <Button size="icon" variant="ghost" className="w-8 h-8" onClick={() => navigate(-1) }>
-              <ChevronLeft size={18} />
-            </Button>
-            <Button size="icon" variant="ghost" className="w-8 h-8" onClick={() => navigate(1) }>
-              <ChevronRight size={18} />
-            </Button>
-          </div>
-          <div className="text-center font-bold text-xm">
-              {match && (<div>
-                {store.bookStack[0]?.title}
-              </div>)}
-          </div>
-          <div className="grid grid-flow-col gap-[2px]">
-            {match && (
-              <Button
-                size="icon"
-                variant="ghost"
-                className="w-8 h-8"
-              >
-                <PanelRight size={18} onClick={() => {}} />
-              </Button>
-            )}
-          </div>
+        <Sidebar />
+        <div className="rounded-lg overflow-hidden shadow-lg bg-white/80">
+          <Outlet />
         </div>
-        {/* <TabBar /> */}
-        <motion.div
-          layout
-          ref={scope}
-          className={clsx("grid grid-cols-[minmax(0,max-content)_1fr]")}
-        >
-          {isReading}
-          <Sidebar />
-          <div className="rounded-lg overflow-hidden">
-            <Outlet />
-            {/* <Home /> */}
-          </div>
-        </motion.div>
-        <p className="hidden">
-          node server status: pid: {server.pid} connected: {server.connected}{" "}
-          signCode: {server.signalCode}
-        </p>
-      </div>
+      </motion.div>
+      <p className="hidden">
+        node server status: pid: {server.pid} connected: {server.connected}{" "}
+        signCode: {server.signalCode}
+      </p>
     </>
   );
 }
--- apps/bookwise/src/components/SideBar/index.tsx
+++ apps/bookwise/src/components/SideBar/index.tsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import { Home, Library, Plus, Search, Star } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 import { useAnimate, motion } from "framer-motion";
+import {
+  ChevronLeft,
+  ChevronRight,
+  PanelLeftClose,
+  PanelLeftOpen,
+} from "lucide-react";
 import { useBearStore } from "@/store";
 import { Button } from "../ui/button";
 import { NavItem } from "./NavItem";
@@ -8,9 +15,15 @@ import "./index.css";
 
 export const Sidebar = () => {
   const [scope, animate] = useAnimate();
+  const navigate = useNavigate();
   const store = useBearStore((state) => ({
     sidebarCollapse: state.sidebarCollapse,
+    updateSidebarCollapse: state.updateSidebarCollapse,
   }));
+
+  const toggleSidebar = () => {
+    store.updateSidebarCollapse();
+  };
 
   useEffect(() => {
     console.log(
@@ -19,19 +32,53 @@ export const Sidebar = () => {
       store.sidebarCollapse
     );
     if (store.sidebarCollapse) {
-      animate([
-        [
-          scope.current, { x: "-100%", width: 0 }, { duration: 0.3 }
-        ],
-      ]);
+      animate([[scope.current, { x: "-100%", width: 0, paddingRight: 0 }, { duration: 0.3 }]]);
     } else {
-      animate(scope.current, { x: "0", width: 276 }, { duration: 0.3 });
+      animate(scope.current, { x: "0", width: 200, paddingRight: '0.5rem' }, { duration: 0.3 });
     }
   }, [store.sidebarCollapse]);
 
   return (
-    <motion.div layout id="sidebar" ref={scope} className=" overflow-hidden">
-      <div className="w-[260px] h-full bg-white/50 shadow-sm rounded-md border border-[#efefef] border-opacity-60">
+    <motion.div layout id="sidebar" ref={scope} className="overflow-hidden pr-2">
+      <div className="grid grid-flow-col gap-[2px]">
+        {!store.sidebarCollapse && (
+          <Button
+            size="icon"
+            variant="ghost"
+            className="w-8 h-8"
+            onClick={toggleSidebar}
+          >
+            <PanelLeftClose size={18} />
+          </Button>
+        )}
+        {store.sidebarCollapse && (
+          <Button
+            size="icon"
+            variant="ghost"
+            className="w-8 h-8"
+            onClick={toggleSidebar}
+          >
+            <PanelLeftOpen size={18} />
+          </Button>
+        )}
+        <Button
+          size="icon"
+          variant="ghost"
+          className="w-8 h-8"
+          onClick={() => navigate(-1)}
+        >
+          <ChevronLeft size={18} />
+        </Button>
+        <Button
+          size="icon"
+          variant="ghost"
+          className="w-8 h-8"
+          onClick={() => navigate(1)}
+        >
+          <ChevronRight size={18} />
+        </Button>
+      </div>
+      <div className="w-full h-full bg-white/50 shadow-sm rounded-md border border-[#efefef] border-opacity-60">
         <div className="p-3 flex flex-row space-x-3">
           <Button
             variant={"secondary"}
