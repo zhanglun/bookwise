ed9ff1af14ef04438a03b6a64a6cd93a04d227ff
zhanglun <zhanglun1410@gmail.com>
2023-09-19 16:58:23 +0800

    Update Catalog

--- apps/bookwise/src/pages/Reader/Catalog.tsx
+++ apps/bookwise/src/pages/Reader/Catalog.tsx
@@ -3,6 +3,8 @@ import { ChevronsLeft } from "lucide-react";
 import clsx from "clsx";
 import { animate, motion } from "framer-motion";
 import { BookCatalog } from "@/helpers/parseEpub";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export interface CatalogProps {
   data: BookCatalog[];
@@ -15,19 +17,13 @@ export const Catalog = (props: CatalogProps) => {
   const { data, packaging, onGoToPage, className } = props;
   const { metadata } = packaging;
   const [expanded, setExpanded] = useState(false);
+  const navigate = useNavigate();
 
   const goToPage = (href: string, id: string) => {
     onGoToPage(href, id);
   };
 
   const renderItems = (list: BookCatalog[], idx = 0) => {
-    const styles = [
-      "font-normal mx-3 py-1",
-      "font-normal ml-6 mr-3 py-1",
-      "font-normal ml-10 mr-3 py-1",
-      "font-normal ml-14 mr-3 py-1",
-    ];
-
     return list.map((item) => {
       const { id, label, href, subitems } = item;
 
@@ -39,7 +35,7 @@ export const Catalog = (props: CatalogProps) => {
             data-anchor-id={id}
             className={clsx(
               "hover:underline hover:text-accent-foreground overflow-hidden text-ellipsis whitespace-nowrap",
-              "py-2 px-3"
+              "pb-4"
             )}
             onClick={() => goToPage(href, id)}
           >
@@ -59,12 +55,22 @@ export const Catalog = (props: CatalogProps) => {
       id="catalog"
       className={clsx("grid grid-flow-row w-[296px] overflow-hidden rounded-s-lg", className)}
     >
+      <div className="sticky top-0 left-0 px-3 py-3 border-b border-border">
+        <Button
+          size="icon"
+          variant="ghost"
+          className="w-8 h-8 text-stone-700 hover:text-stone-900 bg-stone-300/30 hover:bg-stone-500/30"
+          onClick={() => navigate(-1)}
+        >
+          <ChevronsLeft size={18} />
+        </Button>
+      </div>
       <div className="grid grid-flow-col grid-cols-[1fr] gap-1 items-center px-5 py-2 mt-3">
-        <span className="text-md font-bold overflow-hidden whitespace-nowrap text-ellipsis">
+        <span className="text-sm font-bold overflow-hidden whitespace-nowrap text-ellipsis">
           {metadata.title}
         </span>
       </div>
-      <div className="px-2 py-2 h-full overflow-y-scroll">
+      <div className="px-5 py-2 h-full overflow-y-scroll">
         {renderItems(data)}
       </div>
     </motion.div>
