abda18f54d0e2174fc73015fdf6d7494a970fca6
zhanglun <zhanglun1410@gmail.com>
2024-02-05 17:02:50 +0800

    chore: add sonner

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,17 +1,21 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./layout/Sidebar";
+import { Toaster } from 'sonner'
 import "./App.css";
 
 function App() {
   return (
-    <div className="text-foreground bg-background grid h-full grid-cols-[auto_1fr] gap-2 p-3">
-      <div>
-        <Sidebar/>
+    <>
+      <Toaster/>
+      <div className="text-foreground bg-background grid h-full grid-cols-[auto_1fr] gap-2 p-3">
+        <div>
+          <Sidebar/>
+        </div>
+        <div>
+          <Outlet/>
+        </div>
       </div>
-      <div>
-        <Outlet/>
-      </div>
-    </div>
+    </>
   );
 }
 
