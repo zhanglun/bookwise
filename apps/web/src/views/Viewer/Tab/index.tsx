5ab19d6bed14cd0d32e7a79968fda7ca083ac4cb
zhanglun <zhanglun1410@gmail.com>
2024-09-05 16:42:16 +0800

    improvement: add home

--- apps/web/src/views/Viewer/Tab/index.tsx
+++ apps/web/src/views/Viewer/Tab/index.tsx
@@ -1,21 +1,32 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import clsx from "clsx";
 import { useLiveQuery } from "dexie-react-hooks";
-import { FileIcon } from "@radix-ui/react-icons";
+import { FileIcon, HomeIcon } from "@radix-ui/react-icons";
 import { useBook } from "@/hooks/book";
 import { db } from "@/store/db";
+
 import "./index.css";
+import { RouteConfig } from "@/config";
 
 export const ViewTab = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const bookCached = useLiveQuery(async () => {
     return await db.bookCached.toArray();
   });
 
   const { activateBook } = useBook();
 
+  const goToHome = () => {
+    navigate(RouteConfig.HOME);
+  };
+
   return (
     <div className="tab">
+      <div className="tab-item" onClick={() => goToHome()}>
+        <HomeIcon />
+        <span>Home</span>
+      </div>
       {bookCached?.map((item) => {
         const isActive = location.pathname.includes(item.book_id.toString());
 
