7dae63aae777cd61c0ec31187e8f7b191ff93965
zhanglun <zhanglun1410@gmail.com>
2024-09-16 20:20:49 +0800

    improvement: postgres

--- apps/server/src/prisma.service.ts
+++ apps/server/src/prisma.service.ts
@@ -1,8 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PGlite } from '@electric-sql/pglite';
+import { PrismaPGlite } from 'pglite-prisma-adapter';
+
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+  // constructor() {
+  //   const client = new PGlite(connectionString);
+  //   const adapter = new PrismaPGlite(client);
+
+  //   super({ adapter });
+  // }
+
   async onModuleInit() {
     // Note: this is optional
     await this.$connect();
