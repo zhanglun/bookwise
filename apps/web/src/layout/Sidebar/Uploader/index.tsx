d08370571408abbfcf4635a29850c98b1a66c1ac
zhanglun <zhanglun1410@gmail.com>
2024-11-22 18:26:24 +0800

    improvement: parse pdf

--- apps/web/src/layout/Sidebar/Uploader/index.tsx
+++ apps/web/src/layout/Sidebar/Uploader/index.tsx
@@ -1,137 +1,45 @@
 import { FilePlusIcon } from "@radix-ui/react-icons";
 import { IconButton, Tooltip } from "@radix-ui/themes";
-import { getFileFormatType } from "@/helpers/epub";
-import { toast } from "sonner";
-import { BookRequestItem, BookResItem } from "@/interface/book";
-import { Book } from "epubjs";
-import { dal } from "@/dal";
-import { UploadFileBody } from "@/dal/type";
+import { BookResItem } from "@/interface/book";
 import { useEffect } from "react";
 import { IpcRendererEvent } from "electron";
-
-async function formatMetadata(file: File): Promise<[BookRequestItem, string]> {
-  const book = new Book(file as unknown as string);
-  const opened = (await book.opened) as Book & { cover: string };
-  const { cover, packaging } = opened;
-  const { metadata } = packaging;
-
-  return [
-    {
-      title: metadata.title,
-      subject: "",
-      description: metadata.description,
-      contributor: "",
-      identifier: metadata.identifier,
-      source: "",
-      rights: "",
-      language: metadata.language,
-      format: getFileFormatType(file),
-      page_count: 0,
-      isbn: "",
-      authors: metadata.creator,
-      publisher: metadata.publisher,
-      publish_at: new Date(metadata.pubdate),
-    },
-    cover,
-  ];
-}
+import { useFileUpload } from "@/hooks/useFileUpload";
+import { dal } from "@/dal";
+import { toast } from "sonner";
 
 export interface UploaderProps {
   onSuccess: (book: BookResItem) => void;
 }
 
-async function fileReaderAsync(file: File) {
-  return new Promise<string | ArrayBuffer | null>((resolve) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      resolve(reader.result);
-    };
-    reader.readAsArrayBuffer(file); // Read the file as an ArrayBuffer
-  });
-}
-
-async function readFiles(files: File[]) {
-  const body: UploadFileBody[] = [];
-
-  for (const file of files) {
-    const [metadata, cover] = await formatMetadata(file);
-    const buffer = await fileReaderAsync(file);
-    body.push({
-      name: file.name,
-      size: file.size,
-      type: file.type,
-      lastModified: file.lastModified,
-      buffer,
-      metadata,
-      cover,
-    });
-  }
-
-  return body;
-}
-
 export const Uploader = (props: UploaderProps) => {
-  const openFileDialog = (): void => {
-    const input = document.createElement("input");
+  const { isUploading, openFileDialog } = useFileUpload({
+    acceptTypes: [".epub", ".pdf"],
+  });
 
-    input.type = "file";
-    input.multiple = true;
-
-    input.addEventListener(
-      "change",
-      async (e) => {
-        try {
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          const files = e.target.files;
-          console.log(
-            "🚀 ~ file: useBook.ts:9 ~ input.addEventListener ~ files:",
-            files
-          );
-
-          const body = await readFiles(files);
-
-          dal.uploadFile(body);
-
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        } catch (err) {
-          console.log("🚀 ~ err:", err);
-        }
-      },
-      false
-    );
-    console.log("🚀 ~ dal:", dal);
-
-    input.click();
-  };
   useEffect(() => {
     window.electronAPI.onUploadFileSuccess(
       async (_e: IpcRendererEvent, body) => {
-        console.log(
-          "🚀 ~ file: index.tsx:83 ~ window.electronAPI.onUploadFileSuccess ~ body:",
-          body
-        );
         const { model } = body;
         toast.promise(dal.saveBookAndRelations(model), {
           loading: `Uploading ${model.title}`,
           success: (data) => {
-            console.log("%c Line:68 🍖 data", "color:#2eafb0", data);
             props.onSuccess(data);
-            return ` Upload ${model.title} successful`;
+            return `${model.title} uploaded successfully`;
           },
           error: (error) => {
-            return `Upload Error, ${error?.message}`;
+            return `Upload failed: ${error?.message}`;
           },
         });
       }
     );
-  }, []);
+  }, [props]);
 
   return (
     <Tooltip content="Add new book">
       <IconButton
         variant="ghost"
         className="cursor-pointer"
+        disabled={isUploading}
         onClick={openFileDialog}
       >
         <FilePlusIcon width={18} height={18} />
