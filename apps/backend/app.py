8dedb7431463377b0c81700a9d631288e844ef19
zhanglun <zhanglun@yiwise.com>
2025-02-24 22:02:00 +0800

    feat: test flask

---
+++ apps/backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+@app.route("/upload", methods=["POST"])
+def upload_file():
+  if request.method == 'POST':
+    print("f{}", request.form)
+    print("{}", request.files)
+
+    if 'files' not in request.files:
+      return "ok"
+
+    files = request.files.getlist('files')
+
+    for file in files:
+      print(file.filename)
+
+    return 'okk'
